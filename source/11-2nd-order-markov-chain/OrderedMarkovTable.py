50abd7d00e731b01024ff7d79c5801d3252fec83
LinnierGames <esericksanc@gmail.com>
2018-02-11 14:28:07 -0800

    TODO: use queues

--- source/11-2nd-order-markov-chain/OrderedMarkovTable.py
+++ source/11-2nd-order-markov-chain/OrderedMarkovTable.py
@@ -17,46 +17,60 @@ class OrderedMarkovChain(object):
         self.order = order
 
         # iterate corpus
-        previous_word = None
-        for index, current_word in enumerate(self.corpus):
-            adjacent_word = None  # None is equal to stop
+        previous_element = None
+        previous_element_list = None
+        if len(self.corpus) < self.order + 1 and self.order > 0:
+            print("invalid corpus")
+        else:
+            max_iteration = len(self.corpus) - self.order + 1
+            for index in range(0, max_iteration):
+                adjacent_index = index + self.order
+                current_element_list = self.corpus[index:adjacent_index]
+                current_element = ' '.join(current_element_list)
 
-            def get_adjacent_word():
-                try:
-                    return self.corpus[index + self.order]
-                except IndexError:
-                    # end of list_words, thus end of sentence
-                    return None
+                # check: at beginning of sentence, create/append current_word to self.words[None]
+                if index == 0 or previous_element_list[0] in OrderedMarkovChain.end_punctuation:
+                    stochastic_for_beginning_of_sentence = None
+                    try:
+                        stochastic_for_beginning_of_sentence = self.words[None]
+                    except KeyError:
+                        # stochastic for None is not made, so make one
+                        stochastic_for_beginning_of_sentence = Stochastic()
+                        self.words[None] = stochastic_for_beginning_of_sentence
 
-            # check: at beginning of sentence, create/append current_word to self.words[None]
-            if index == 0 or previous_word in OrderedMarkovChain.end_punctuation:
-                stochastic_for_beginning_of_sentence = None
-                try:
-                    stochastic_for_beginning_of_sentence = self.words[None]
-                except KeyError:
-                    # stochastic for None is not made, so make one
-                    stochastic_for_beginning_of_sentence = Stochastic()
-                    self.words[None] = stochastic_for_beginning_of_sentence
+                    stochastic_for_beginning_of_sentence.add_count(current_element)
 
-                stochastic_for_beginning_of_sentence.add_count(current_word)
+                # grab adjacent word, can be None to represent end of the sentence
 
-            # grab adjacent word, can be None to represent end of the sentence
-            adjacent_word = get_adjacent_word()
+                def get_adjacent_word():
+                    try:
+                        adjacent_word = self.corpus[adjacent_index]
+                        temp_list = current_element_list[1:] # current_element without the first index
+                        temp_element = ' '.join(temp_list)
 
-            # if current_word is an ending puncuation, then adjacent_word is None
-            if current_word in OrderedMarkovChain.end_punctuation:
-                adjacent_word = None
-                continue
+                        return temp_element + ' ' + adjacent_word if len(temp_element) != 0 else adjacent_word
+                    except IndexError:
+                        # end of list_words, thus end of sentence
+                        return None
 
-            stochastic_for_current_word = None
-            if current_word in self.words:
-                stochastic_for_current_word = self.words[current_word]
-            else:
-                stochastic_for_current_word = Stochastic()
-                self.words[current_word] = stochastic_for_current_word
+                adjacent_element = get_adjacent_word()
+                print(current_element, "->{}".format(adjacent_element))
 
-            stochastic_for_current_word.add_count(adjacent_word)
-            previous_word = current_word
+                # if current_word is an ending puncuation, then adjacent_word is None
+                if current_element_list[-1] in OrderedMarkovChain.end_punctuation:
+                    adjacent_element = None
+                    # continue
+
+                stochastic_for_current_word = None
+                if current_element in self.words:
+                    stochastic_for_current_word = self.words[current_element]
+                else:
+                    stochastic_for_current_word = Stochastic()
+                    self.words[current_element] = stochastic_for_current_word
+
+                stochastic_for_current_word.add_count(adjacent_element)
+                previous_element = current_element
+                previous_element_list = current_element_list
 
     def generate_a_sentence(self):
         sentence = ""
@@ -64,24 +78,34 @@ class OrderedMarkovChain(object):
         if len(self.words) == 0:
             return sentence
 
-        current_markov_word = None
+        current_markov_element = None
+
+        # start sentence with None frequency
+        current_markov_element = self.words[None].choose_random_word_from_frequency()
+        sentence += current_markov_element
 
         while True: # do-while
-            stochastic = self.words[current_markov_word]
-            current_markov_word = stochastic.choose_random_word_from_frequency()
+            print("s: {}".format(sentence))
+            print("k: {}".format(current_markov_element))
+            stochastic = self.words[current_markov_element]
+            current_markov_element = stochastic.choose_random_word_from_frequency()
 
             # check: end of sentence
-            if current_markov_word is None or current_markov_word in OrderedMarkovChain.end_punctuation:
+            if current_markov_element is None or current_markov_element in OrderedMarkovChain.end_punctuation:
                 # check: by a puncuation? if so, add it to the sentence
-                if current_markov_word in OrderedMarkovChain.end_punctuation:
-                    sentence += current_markov_word
+                if current_markov_element in OrderedMarkovChain.end_punctuation:
+                    sentence += current_markov_element
                 break
             else:
-                sentence += " " + current_markov_word
+                pass
+            current_markov_element_list = current_markov_element.split()
+            sentence += " " + current_markov_element_list[-1]
+
 
         return sentence
 
 
 if __name__ == '__main__':
-    m = OrderedMarkovChain("one fish one", 1)
-    print(m.generate_a_sentence())
+    m = OrderedMarkovChain("one fish two fish red fish . blue fish .", 2)
+    print(m.words)
+    print("Sentence: {}".format(m.generate_a_sentence()))
