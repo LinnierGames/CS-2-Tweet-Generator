4890176e99e9ffabca32a39a83c05a1a430c721a
LinnierGames <esericksanc@gmail.com>
2018-02-13 11:38:27 -0800

    nth-ordered markov

--- source/11-2nd-order-markov-chain/OrderedMarkovTable.py
+++ source/11-2nd-order-markov-chain/OrderedMarkovTable.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import collections
 
 sys.path.insert(0, "../4-exercies")
 from stochastic import Stochastic
@@ -14,17 +15,17 @@ class OrderedMarkovChain(object):
     def __init__(self, words, order):
         self.corpus = re.findall(r"[\w']+|[.,!?;]", words)
         self.words = HashTable(32)
-        self.order = order
+        self.markov_order = order
 
         # iterate corpus
         previous_element = None
         previous_element_list = None
-        if len(self.corpus) < self.order + 1 and self.order > 0:
+        if len(self.corpus) < self.markov_order + 1 and self.markov_order > 0:
             print("invalid corpus")
         else:
-            max_iteration = len(self.corpus) - self.order + 1
+            max_iteration = len(self.corpus) - self.markov_order + 1
             for index in range(0, max_iteration):
-                adjacent_index = index + self.order
+                adjacent_index = index + self.markov_order
                 current_element_list = self.corpus[index:adjacent_index]
                 current_element = ' '.join(current_element_list)
 
@@ -78,37 +79,38 @@ class OrderedMarkovChain(object):
         if len(self.words) == 0:
             return sentence
 
-        current_markov_element = None
+        current_word = None
 
         # start sentence with None frequency
-        current_markov_element = self.words[None].choose_random_word_from_frequency()
-        sentence += current_markov_element
+        current_word = self.words[None].choose_random_word_from_frequency()
+        sentence += current_word
 
-        prev = current_markov_element.split()[-2]
-        curr = current_markov_element.split()[-1]
+        current_word_list = current_word.split()
+
+        prev_queue = collections.deque(current_word_list[:-1], maxlen=self.markov_order - 1)
+        current_word = current_word_list[-1]
 
         while True:  # do-while
-            key = prev + " " + curr
+            prev_word = ' '.join(list(prev_queue))
+            key = prev_word + " " + current_word if len(prev_word) != 0 else current_word
+            prev_queue.append(current_word)
             stochastic = self.words[key]
-            current_markov_element = stochastic.choose_random_word_from_frequency()
+            current_word = stochastic.choose_random_word_from_frequency()
 
             # check: end of sentence
-            if current_markov_element is None or current_markov_element in OrderedMarkovChain.end_punctuation:
+            if current_word is None or current_word in OrderedMarkovChain.end_punctuation:
                 # check: by a puncuation? if so, add it to the sentence
-                if current_markov_element in OrderedMarkovChain.end_punctuation:
-                    sentence += current_markov_element
+                if current_word in OrderedMarkovChain.end_punctuation:
+                    sentence += current_word
                 break
             else:
                 pass
-            sentence += " " + current_markov_element
-
-            prev = curr
-            curr = current_markov_element
+            sentence += " " + current_word
 
         return sentence
 
 
 if __name__ == '__main__':
-    m = OrderedMarkovChain("one fish two fish red fish. blue fish.", 2)
+    m = OrderedMarkovChain("one fish two fish red fish blue fish.", 4)
     print(m.words)
     print("Sentence: {}".format(m.generate_a_sentence()))
